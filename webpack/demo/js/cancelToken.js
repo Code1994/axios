cfab02500b3ea96eaf32c7bd3baba9ab58c7fdf7
yangxiaoping <2878847069@qq.com>
2022-10-14 13:00:39 +0800

    feat: then的执行

--- webpack/demo/js/cancelToken.js
+++ webpack/demo/js/cancelToken.js
@@ -30,5 +30,24 @@ source.cancel('Operation canceled by the user.');
 
 
 
+/*
+  执行then方法之后，再将then方法重新赋值为then2，其执行顺序
+  1.then2先执行，因为此处then是微任务，所以打印结果是then2在then之前
+  2.但可以发现then的打印是按照then函数走的，而并不是then2函数
+*/
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('success')
+//   })
+// })
+// then会先执行 但结果会延迟
+// p.then(res => {
+//   console.log(res)
+// })
 
+// p.then = function (callback) {
+//   console.log(callback)
+// }
+
+// p.then()
 
